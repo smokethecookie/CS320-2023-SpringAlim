40a21e0ea69b748c72d15fb3b6c03bcff0262de8
smokethecookie <ackura@bu.edu>
2023-02-07 18:56:51 -0500

    Test1 passed

--- assigns/02/MySolution/assign02_01.py
+++ assigns/02/MySolution/assign02_01.py
@@ -11,4 +11,29 @@ print("[import ./../assign02.py] is done!")
 # mylist_rappend (see list_rappend in assign02.sml)
 # mylist_reverse (see list_reverse in assign02.sml)
 #
+
+def mylist_append(xs, ys):
+    def helperAppend(cs, x1):
+        if (x1.get_ctag() == 0):
+            return cs
+        else:
+            new = helperAppend(cs, x1.get_cons2())
+            return mylist_cons(x1.get_cons1(), new)
+    
+    return helperAppend(xs, ys)
+
+def mylist_rappend(xs, ys):
+    def helperRappend(cs, x1):
+        if (x1.get_ctag() == 0):
+            return cs
+        else:
+            new = helperRappend(mylist_cons(x1.get_cons1(), cs), x1.get_cons2())
+            return new
+    
+    return helperRappend(xs, ys)
+
+def mylist_reverse(xs):
+    return mylist_rappend(mylist_nil(), xs)
+
+
 ####################################################
